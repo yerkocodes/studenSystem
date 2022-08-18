0442acafcad855b841f062a20bc7892fc24ee3ab
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-08-18 01:24:01 -0400

    ComunaService implementacion created

---
+++ src/main/java/com/studentsystem/serviceImpl/ComunaServiceImpl.java
@@ -0,0 +1,99 @@
+package com.studentsystem.serviceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.studentsystem.ExamenCertificacionJavaStudentSystemApplication;
+import com.studentsystem.dao.IComunaRepository;
+import com.studentsystem.model.Comuna;
+import com.studentsystem.service.IComunaService;
+
+@Service
+public class ComunaServiceImpl implements IComunaService {
+	
+	@Autowired
+	private IComunaRepository dao;
+	
+	private Logger log = LoggerFactory.getLogger(ExamenCertificacionJavaStudentSystemApplication.class);
+
+	@Override
+	@Transactional
+	public Comuna add(Comuna comuna) {
+		Comuna comunaToAdd = new Comuna();
+		try {
+			comunaToAdd = dao.save(comuna);
+		} catch (Exception e) {
+			log.error("Ocurred an error in 'add'. Location: 'com.studentsystem.serviceImpl'. Error: " + e.getMessage());
+		}
+		return comunaToAdd;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Comuna> findAllComunas() {
+		List<Comuna> comunasList = new ArrayList<>();
+		try {
+			comunasList = (List<Comuna>) dao.findAll();
+		} catch (Exception e) {
+			log.error("Ocurred an error in 'findAllComunas'. Location: 'com.studentsystem.serviceImpl'. Error: " + e.getMessage());
+		}
+		return comunasList;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Comuna findById(Integer id) {
+		Comuna estudiante = new Comuna();
+		try {
+			estudiante = dao.findById(id).get();
+		} catch (Exception e) {
+			log.error("Ocurred an error in 'findById'. Location: 'com.studentsystem.serviceImpl'. Error: " + e.getMessage());
+		}
+		
+		return estudiante;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Comuna findByRut(String rut) {
+		return null;
+	}
+
+	@Override
+	@Transactional
+	public Comuna update(Comuna comuna) {
+		Comuna comunaToUpdate = new Comuna();
+		try {
+			
+			comunaToUpdate = dao.save(comuna);
+			
+		} catch (Exception e) {
+			log.error("Ocurred an error in 'update'. Location: 'com.studentsystem.serviceImpl'. Error: " + e.getMessage());
+		}
+		return comunaToUpdate;
+	}
+
+	@Override
+	@Transactional
+	public Boolean deleteComuna(Integer id) {
+		try {
+			if (dao.findById(id) != null) {
+				dao.deleteById(id);
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			log.error("Ocurred an error in 'deleteComuna'. Location: 'com.studentsystem.serviceImpl'. Error: " + e.getMessage());
+		}
+		return false;
+	}
+
+}
