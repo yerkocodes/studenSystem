eb1904eb447007146770b502343fcfe45a81145b
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-08-18 01:39:02 -0400

    Added custom findByRutEstudiante JPA method into IEstudianteRepository

--- src/main/java/com/studentsystem/dao/IEstudianteRepository.java
+++ src/main/java/com/studentsystem/dao/IEstudianteRepository.java
@@ -1,9 +1,13 @@
 package com.studentsystem.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.studentsystem.model.Estudiante;
 
 public interface IEstudianteRepository extends CrudRepository<Estudiante, Integer> {
+	
+	@Query("SELECT * FROM Estudiante WHERE rut = :rutEstudiante")
+	public Estudiante findByRutEstudiante(String rutEstudiante);
 
 }
