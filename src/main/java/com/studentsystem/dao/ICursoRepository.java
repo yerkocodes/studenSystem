a5b42fbf2ece491fa611419bc86fbf26aa147609
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-08-18 02:00:39 -0400

    Added custom findByIdCurso JPA method into ICursoRepository

--- src/main/java/com/studentsystem/dao/ICursoRepository.java
+++ src/main/java/com/studentsystem/dao/ICursoRepository.java
@@ -1,9 +1,13 @@
 package com.studentsystem.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.studentsystem.model.Curso;
 
 public interface ICursoRepository extends CrudRepository<Curso, String> {
+	
+	@Query("SELECT * FROM Curso WHERE codigo_curso = :codidoDelCurso")
+	public Curso findByIdCurso(String codigoDelCurso);
 
 }
