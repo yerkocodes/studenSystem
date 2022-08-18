54f140a99984ce57009c54746d0170bb230313a1
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-08-17 22:44:56 -0400

    Fist Commit

---
+++ src/main/java/com/studentsystem/ExamenCertificacionJavaStudentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.studentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenCertificacionJavaStudentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenCertificacionJavaStudentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentsystem/ExamenCertificacionJavaStudentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenCertificacionJavaStudentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
